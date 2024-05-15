68f5b87431618c8b89847d1f0c3780d123f0e13d
LaDanielHa <laurentiusdaniel0@gmail.com>
2024-05-15 14:36:31 +0700

    student

--- app/Http/Middleware/ApiMiddleware.php
+++ app/Http/Middleware/ApiMiddleware.php
@@ -3,8 +3,10 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Exception;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class ApiMiddleware
 {
@@ -15,12 +17,17 @@ class ApiMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if ($request->expectsJson()) {
-            return $next($request);
+        try {
+            $user = JWTAuth::parseToken()->authenticate();
+        } catch (Exception $e) {
+            if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException) {
+                return response()->json(['status' => 'Token is Invalid']);
+            } else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException) {
+                return response()->json(['status' => 'Token is Expired']);
+            } else {
+                return response()->json(['status' => 'Authorization Token not found']);
+            }
         }
-
-        // You can implement any other logic here
-
-        return response()->json(['error' => 'Unauthorized'], 401);
+        return $next($request);
     }
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\ApiMiddleware;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -20,10 +21,11 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
         $middleware->group('api', [
+            \App\Http\Middleware\ApiMiddleware::class
         ]);
 
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-         //
+        //
     })->create();
